885d5c9f92ecaabb628f922b760f6c44cb77e3b6
aalnoman10 <aalnomanm10@gmail.com>
2023-04-28 16:36:36 +0600

    update dynamic link

--- src/pages/Sheared/LeftBar/LeftBar.jsx
+++ src/pages/Sheared/LeftBar/LeftBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftBar = () => {
+
+    const [cates, setCates] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/cate')
+            .then(res => res.json())
+            .then(data => setCates(data))
+            .catch(e => {
+                console.error(e);
+            })
+    }, [])
+
     return (
         <div>
-            LeftBar
+            <h3 className="text0-bold">All Category</h3>
+            <div>
+                {
+                    cates.map(cate => {
+                        return <p key={cate.id}>
+                            <Link to={`cate/${cate.id}`}
+                                className='text-secondary text-decoration-none d-block'
+                            >{cate.name}</Link>
+                        </p>
+                    })
+                }
+            </div>
         </div>
     );
 };
